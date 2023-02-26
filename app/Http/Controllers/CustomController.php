a4fb668f8722f3c339145c50f4c7662c6c885bdd
zhaozhao <zhaozhao@zhaozhaodeMac-mini.local>
2023-02-26 10:37:41 +0800

    codeclimate gogogo

--- app/Http/Controllers/CustomController.php
+++ app/Http/Controllers/CustomController.php
@@ -21,6 +21,7 @@ use App\Models\SystemTeam;
 use App\Models\SystemUser;
 use App\Providers\AppServiceProvider;
 use App\Services\ApproveService;
+use App\Services\CustomerEditService;
 use App\Services\CustomerService;
 use App\Services\RightService;
 use App\Services\ToolService;
@@ -221,119 +222,23 @@ class CustomController extends Controller
 
         $operUserId = $request->session()->get('user_id');
 
-        $customModel = new Customer();
+        $service = app(CustomerEditService::class);
 
         DB::beginTransaction();
 
         if (isset($params['id'])) {
-            $customId = $params['id'];
-            $service = app(CustomerService::class);
-            $customModel = $customModel->find($params['id']);
-            $oldInfo = clone ($customModel);
-            $params['mobile_md5'] = md5($params['mobile']);
-            $flag = $customModel->update($params);
-            if (!$flag) {
-                DB::rollBack();
-                return $this->apiReturn(static::ERROR, [], "基本信息保存失败");
-            }
-            $diff = $service->diff($oldInfo, $customModel);
-            if ($diff['star']) {
-                $logModel = new CustomerLog();
-                $flag = $logModel->saveLog($logModel::TYPE_STAR, $params['id'], $diff['star']['old'], $diff['star']['new'], $operUserId, $customModel->remark);
-                if (!$flag) {
-                    DB::rollBack();
-                    return $this->apiReturn(static::ERROR, [], "星级变化保存失败");
-                }
-            }
-            if ($diff['follow_status']) {
-                $logModel = new CustomerLog();
-                $flag = $logModel->saveLog($logModel::TYPE_FOLLOW, $params['id'], $diff['follow_status']['old'], $diff['follow_status']['new'], $operUserId, $customModel->remark);
-                if (!$flag) {
-                    DB::rollBack();
-                    return $this->apiReturn(static::ERROR, [], "跟进变化保存失败");
-                }
-                $customModel->follow_time = time();
-                $customModel->save();
-                if (!$flag) {
-                    DB::rollBack();
-                    return $this->apiReturn(static::ERROR, [], "跟进时间保存失败");
-                }
-            } else if (!empty($params['remark'])) {
-                $logModel = new CustomerLog();
-                $flag = $logModel->saveLog($logModel::TYPE_FOLLOW, $params['id'], intval($params['follow_status']), intval($params['follow_status']), $operUserId, $params['remark']);
-                if (!$flag) {
-                    DB::rollBack();
-                    return $this->apiReturn(static::ERROR, [], "跟进变化保存失败");
-                }
-                $customModel->follow_time = time();
-                $customModel->save();
-                if (!$flag) {
-                    DB::rollBack();
-                    return $this->apiReturn(static::ERROR, [], "跟进时间保存失败");
-                }
-            }
-            unset($diff['star']);
-            unset($diff['follow_status']);
-            if (count($diff) > 0) {
-                $logModel = new CustomerLog();
-                $flag = $logModel->saveLog($logModel::TYPE_EDIT, $params['id'], '', '', $operUserId, '');
-                $logmodel = new SystemLog();
-                $logmodel->saveLog($logmodel::TYPE_EDIT, $params['id'], json_encode($diff), '', $operUserId, strval($params['config']['remark']));
-            }
-            $customId = $params['id'];
+            $result = $service->edit($params, $operUserId);
         } else {
-            $params['add_user_id'] = $operUserId;
-            $params['apply_time'] = time();
-            $params['assign_time'] = time();
-            // 转介绍
-            $params['user_from'] = empty($params['introid']) ? CustomerService::ASSIGN_TYPE_SELF : CustomerService::ASSIGN_TYPE_INTRO;
-            $params['follow_user_id'] = $operUserId;
-            $params['mobile_md5'] = md5($params['mobile']);
-            if ($params['follow_status']) {
-                $params['follow_time'] = time();
-            }
-            $custom = $customModel->create($params);
-            if (!$custom->id) {
-                DB::rollBack();
-                return $this->apiReturn(static::ERROR, [], "客户录入失败");
-            }
-            $logModel = new CustomerLog();
-            if ($params['introid']) {
-                $flag = $logModel->saveLog($logModel::TYPE_INTRO, $custom->id, $params['introid'], '', $operUserId, "转介绍");
-                $flag = $logModel->saveLog($logModel::TYPE_INTRO, $params['introid'], $custom->id, '', $operUserId, "转介绍");
-            } else {
-                $flag = $logModel->saveLog($logModel::TYPE_IN, $custom->id, '', '', $operUserId, "手动录入");
-            }
-            if (!$flag) {
-                DB::rollBack();
-                return $this->apiReturn(static::ERROR, [], "客户录入失败.");
-            }
-            if ($params['star']) {
-                $logModel = new CustomerLog();
-                $flag = $logModel->saveLog($logModel::TYPE_STAR, $custom->id, 0, $params['star'], $operUserId, strval($params['remark']));
-                if (!$flag) {
-                    DB::rollBack();
-                    return $this->apiReturn(static::ERROR, [], "星级变化保存失败");
-                }
-            }
-            if ($params['follow_status']) {
-                $logModel = new CustomerLog();
-                $flag = $logModel->saveLog($logModel::TYPE_FOLLOW, $custom->id, 0, $params['follow_status'], $operUserId, strval($params['remark']));
-                if (!$flag) {
-                    DB::rollBack();
-                    return $this->apiReturn(static::ERROR, [], "跟进变化保存失败");
-                }
-            }
-            
-            $customId = $custom->id;
+            $result = $service->add($params, $operUserId);
         }
 
-        if ($params['remark']) {
-            $customerRemarkLogModel = new CustomerRemarkLog();
-            $customerRemarkLogModel->saveLog($customId, $params['remark'], $operUserId);
+        if ($result['status'] == 1) {
+            DB::commit();
+        } else {
+            DB::rollBack();
+            return $this->apiReturn(static::ERROR, [], $result['error']);
         }
 
-        DB::commit();
         return $this->apiReturn(static::OK, [], "操作成功");
     }
 
